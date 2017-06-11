8763cf1660983891a320c96337714239c25a7ea9
jordangw <jordan.williams@revera.co.nz>
2017-06-12 11:56:47 +1200

    the pro group
    pro group

---
+++ theprogroup.co.nz/xmlrpc0db0.php
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>https://wordpress.org/</engineLink>
+    <homePageLink>http://theprogroup.co.nz</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="http://theprogroup.co.nz/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://theprogroup.co.nz/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://theprogroup.co.nz/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="http://theprogroup.co.nz/xmlrpc.php" />
+      	<api name="WP-API" blogID="1" preferred="false" apiLink="http://theprogroup.co.nz/wp-json/" />
+	    </apis>
+  </service>
+</rsd>
